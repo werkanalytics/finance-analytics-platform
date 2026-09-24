1fd1b6a9d24a35086d4df39761d17d20ae991e29
Ahmet Fırat Küçük <firatahmetkucuk@gmail.com>
2026-09-24 20:52:37 +0300

    Move database credentials to environment variables

--- etl/database.py
+++ etl/database.py
@@ -1,6 +1,18 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
+load_dotenv()
+
+
 def get_engine():
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+    port = os.getenv("POSTGRES_PORT")
+    database = os.getenv("POSTGRES_DB")
+
     return create_engine(
-        "postgresql+psycopg2://postgres:ahmet2908@host.docker.internal:5432/postgres"
+        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
     )
